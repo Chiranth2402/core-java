b9c08cc4e748fa48de3988bac035028ca7b1f16a
Chiranth G H <chiranthvishwakarma@gmail.com>
2026-08-11 12:18:30 +0530

    Uploaded by chiranth

---
+++ 18july/classassignment.java
@@ -0,0 +1,105 @@
+class MyInformation{
+	public static void main(String[] args){
+		System.out.println("Hii,My name is Chiranth...");
+		System.out.println("I am from Kodagu,Karnataka");
+		System.out.println("I have completed my BE from MVJ college of Engineering");
+		System.out.println("I am intrested in java programing,software development and Data Analytics");
+		System.out.println("I am looking for an opportunity where i can apply my skills,learn and grow professionally");
+	}
+}
+
+class MycollegeInfo{
+	public static void main(String[] args){
+		System.out.println("MVJ college of Engineering is located in Bengaluru,Karnataka");
+		System.out.println("MVJ college of Engineering Established in 1984");
+		System.out.println("It is one of the well known Engineering college in Karnataka");
+		System.out.println("The College Offers Undergraduate and Postgraduate program in various Engineering Disciplines ");
+		System.out.println("Our college focusses on techincal knowledge,practical learning and Sports");
+	}
+}
+
+class EducationDetails{
+	public static void main(String[] args){
+		System.out.println("I have Completed my 'X' from Atal Bihari Vajpayee School with 86%");
+		System.out.println("I have completed my 'XII' from Morarji Desai Pre University College with 93%");
+		System.out.println("I have completed my Engineering Degree from MVJ college of Engineering with 8.5gpa");
+	}
+}
+
+class RelationshipDetails{
+	public static void main(String[] args){
+		System.out.println("My name is Chiranth");
+		System.out.println("My Mother name is Revathi");
+		System.out.println("My Father name is Hoovannachar");
+		System.out.println("My Brother name is Yashwanth");
+	}
+}
+
+class DreamJob{
+	public static void main(String[] args){
+		System.out.println("My dream job is to become a software Enginner");
+		System.out.println("I want to work in a Reputed Software MNC Compnay");
+		System.out.println("My goal is to build a useful software, solve realworld peoblems,and grow in my career");
+	}
+}
+
+class MylaptopInfo{
+	public static void main(String[] args){
+		System.out.println("I use an ASUS TUF Gaming F15 laptop");
+	    System.out.println("It is powerd by an Intel Core i5 processor");
+	    System.out.println("it also has NVIDIA GeForce GTX graphics");
+	    System.out.println("I use this for my academic preposes");
+	    System.out.println("I like my Laptop");
+	}
+}
+
+
+class MyMobileInfo{
+	public static void main(String[] args){
+		System.out.println("I use an iQOO Neo 10R smartphone");
+		System.out.println("It has a powerful processor");
+		System.out.println("It comes with a good quality camera");
+		System.out.println("I use my mobile for learning and communication");
+		System.out.println("I like my mobile phone");
+	}
+}
+
+class MyTVInfo{
+	public static void main(String[] args){
+		System.out.println("We have a Smart LED TV at home");
+		System.out.println("It provides good picture quality");
+		System.out.println("My family watches movies and news on it");
+		System.out.println("It also supports YouTube and OTT applications");
+		System.out.println("I enjoy watching cricket on TV");
+	}
+}
+
+class MyHobbies{
+	public static void main(String[] args){
+		System.out.println("My hobbies are playing cricket and volleyball");
+		System.out.println("I enjoy listening to music");
+		System.out.println("I like reading books");
+		System.out.println("I spend time learning new technologies");
+		System.out.println("My hobbies help me stay active and relaxed");
+	}
+}
+
+class MyFavoriteFood{
+	public static void main(String[] args){
+		System.out.println("My favorite food is Chicken Biryani");
+		System.out.println("I also like South Indian dishes");
+		System.out.println("I enjoy eating homemade food");
+		System.out.println("I like trying different cuisines");
+		System.out.println("Good food keeps me happy");
+	}
+}
+
+class MyFavoritePlace{
+	public static void main(String[] args){
+		System.out.println("My favorite place is Kodagu");
+		System.out.println("It is famous for its coffee plantations");
+		System.out.println("The weather is pleasant throughout the year");
+		System.out.println("It has beautiful hills and waterfalls");
+		System.out.println("I enjoy spending time there with my family");
+	}
+}
