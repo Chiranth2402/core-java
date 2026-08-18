1297f2aba3bc0ea6611b3d906e2bbd47198ba334
Chiranth G H <chiranthvishwakarma@gmail.com>
2026-08-18 22:41:16 +0530

    initialization and declaring the array using litterals

---
+++ Arrays/MobileNumberRunner.java
@@ -0,0 +1,29 @@
+class MobileNumberRunner{
+	public static void main(String[] args){
+		
+		long chiranthPhoneNumber=6362520438l;
+		long varunPhoneNumber=9876543210l;
+		long suhasPhoneNumber=8975642310l;
+		long rachanaPhoneNumber=7985461320l;
+		long kavyaPhoneNumber=8897556412l;
+		long yashwanthPhoneNumber=7766589412l;
+		long chandanPhoneNumber=8897456524l;
+		long sujanPhoneNumber=5546897456l;
+		long sachinPhoneNumber=8779546877l;
+		long sannithPhoneNumber=7898564587l;
+		
+		//10 exp;icit refference
+		
+		long [] mobileNumbers={chiranthPhoneNumber,varunPhoneNumber,suhasPhoneNumber,
+		yashwanthPhoneNumber,kavyaPhoneNumber,
+		rachanaPhoneNumber,sannithPhoneNumber,sujanPhoneNumber,
+		chandanPhoneNumber,sachinPhoneNumber,
+		7782256423l,8895568784l,5589745689l,5589745689l,
+		8897745894l,8897884568l,5546778985l,
+		8898789546l,889568974l,8856987457l};//10 implicit refference
+		
+		int numberOfElememtsInAnArray=mobileNumbers.length;
+		
+		System.out.println("The Number of elements in mobileNumbers Array are:"+numberOfElememtsInAnArray);
+	}
+}
